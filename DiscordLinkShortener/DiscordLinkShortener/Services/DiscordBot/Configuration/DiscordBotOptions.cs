967d1c183d456b968999ae9a2190a50e191b0649
NightFish <NightFish.net@gmail.com>
2023-01-30 22:44:06 +1300

    Add discord bot config

---
+++ DiscordLinkShortener/DiscordLinkShortener/Services/DiscordBot/Configuration/DiscordBotOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace DiscordLinkShortener.Services.DiscordBot.Configuration;
+
+public class DiscordBotOptions
+{
+    public const string Name = "DiscordBot";
+
+    public string Token { get; set; } = string.Empty;
+    public string StatusText { get; set; } = string.Empty;
+}
---
+++ DiscordLinkShortener/DiscordLinkShortener/Services/DiscordBot/Configuration/DiscordBotOptionsValidation.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace DiscordLinkShortener.Services.DiscordBot.Configuration;
+
+public class DiscordBotOptionsValidation : IValidateOptions<DiscordBotOptions>
+{
+    public ValidateOptionsResult Validate(string name, DiscordBotOptions options)
+    {
+        if (string.IsNullOrWhiteSpace(options.Token))
+        {
+            return ValidateOptionsResult.Fail("Missing Discord bot token.");
+        }
+
+        return ValidateOptionsResult.Success;
+    }
+}
