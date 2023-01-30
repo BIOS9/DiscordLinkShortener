5450ba9b128660408838a7d22a24a3e1f68b0324
NightFish <NightFish.net@gmail.com>
2023-01-31 01:25:46 +1300

    Add aliexpress link shortener

--- src/Services/LinkShortener/Shorteners/AliexpressShortener.cs
+++ src/Services/LinkShortener/Shorteners/AliexpressShortener.cs
@@ -1,14 +1,20 @@
-﻿namespace DiscordLinkShortener.Services.LinkShortener.Shorteners;
+﻿using System.Text.RegularExpressions;
+
+namespace DiscordLinkShortener.Services.LinkShortener.Shorteners;
 
 public class AliexpressShortener : ILinkShortener
 {
-    public bool CanShorten(string link)
-    {
-        throw new NotImplementedException();
-    }
+    private static readonly Regex _aliLinkRegex =
+        new(@"^https?:\/\/(?:[^\s]+.)?aliexpress.com\/item\/([0-9]+).+$", RegexOptions.IgnoreCase);
+
+    public bool CanShorten(string link) => _aliLinkRegex.IsMatch(link);
 
     public Task<string> ShortenAsync(string link)
     {
-        throw new NotImplementedException();
+        var m = _aliLinkRegex.Match(link);
+        if (!m.Success)
+            throw new ArgumentException("Invalid Aliexpress link.");
+
+        return Task.FromResult($"https://aliexpress.com/item/{m.Groups[1].Value}.html");
     }
 }
